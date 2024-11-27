2423f00e8d204293e963c995b1cc468ca45b83c1
AndreaDiazZagal <20213tn002@utez.edu.mx>
2024-11-26 20:22:26 -0600

    cambios de coches

--- gpds-autos/src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
+++ gpds-autos/src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/automoviles")
+@RequestMapping("/api/coches")
 public class AutomovilController {
 
     @Autowired
